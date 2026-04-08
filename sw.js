fefa8d342c61ed79077d58b5e304d3782a717c33
mvjyrou <matiushkinvolodymyr@gmail.com>
2026-04-08 19:31:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
-self.addEventListener('fetch', (event) => {
-	event.respondWith(
-		fetch(event.request).catch(() => {
-		return caches.match(event.request);
-		})
-	);
+self.addEventListener('install', (event) => {
+	console.log('Service Worker установлен');
+});
+self.addEventListener('activate', (event) => {
+	console.log('Service Worker активирован');
 });
